449a5442fcd1a7fa236579a60d770a6c17d49343
Ethan Glenn <ethan+git@eglenn.dev>
2025-01-22 21:48:37 -0700

    feat: Implement NoteSidebar component and integrate it into the note editing page

--- app/(authenticated)/note/[postId]/page.tsx
+++ app/(authenticated)/note/[postId]/page.tsx
@@ -2,10 +2,20 @@ import { getSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 import { getPostByKey } from "@/models/post-model";
 import Note from "@/components/note";
+import NoteSidebar from "@/components/note-sidebar";
+import { getPostsByUser } from "@/models/post-model";
 
 export const metadata = {
     title: "Edit Note | MarkNote.one",
 };
+
+interface Post {
+    id: string;
+    title: string;
+    content: string;
+    userId: string;
+    lastUpdated: string;
+}
 
 export default async function Page({
     params,
@@ -19,11 +29,26 @@ export default async function Page({
     if (!post) redirect("/");
     if (session.user.userId !== post.userId) redirect("/");
 
+    const posts = (await getPostsByUser(session.user.userId)) as Post[];
+    if (!posts) return [];
+    const postsArray = Object.entries(posts).map(([key, post]) => {
+        return {
+            id: key,
+            title: post.title,
+            content: post.content,
+            userId: post.userId,
+            lastUpdated: post.lastUpdated,
+        };
+    }) as Post[];
+
     return (
-        <Note
-            userId={session.user.userId}
-            postKey={(await params).postId}
-            post={post}
-        />
+        <div className="flex flex-row h-full gap-3">
+            <NoteSidebar posts={postsArray} />
+            <Note
+                userId={session.user.userId}
+                postKey={(await params).postId}
+                post={post}
+            />
+        </div>
     );
 }
---
+++ components/note-sidebar.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { File } from "lucide-react";
+
+interface Post {
+    id: string;
+    title: string;
+    content: string;
+    userId: string;
+    lastUpdated: string;
+}
+
+interface EditorSidebarProps {
+    posts: Post[];
+}
+
+export default function NoteSidebar({ posts }: EditorSidebarProps) {
+    const [menuOpen, setMenuOpen] = useState(true);
+
+    if (posts.length === 0) {
+        return (
+            <div className="flex flex-col items-center justify-center w-56 h-full gap-3">
+                <h3 className="text-lg font-bold text-center">Files</h3>
+                <p className="text-xs text-center">No files, yet!</p>
+            </div>
+        );
+    }
+
+    return (
+        <div
+            className={`${
+                menuOpen ? "w-56" : "w-11"
+            } h-full border-gray-200 select-none transition-all duration-300`}
+        >
+            <div className="flex items-center p-2 gap-2s">
+                <div onClick={() => setMenuOpen(!menuOpen)}>
+                    {menuOpen ? <OpenedMenu /> : <ClosedMenu />}
+                </div>
+                <h3
+                    className={
+                        menuOpen ? "text-lg font-bold text-center" : "hidden"
+                    }
+                >
+                    Files
+                </h3>
+            </div>
+            <div className={menuOpen ? "h-full" : "hidden"}>
+                {posts.map((post) => (
+                    <Link key={post.title} href={`/note/${post.id}`}>
+                        <Button variant="ghost" className="text-xs">
+                            <span>{">"}</span>
+                            <File size={16} />
+                            {post.title}
+                        </Button>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function ClosedMenu() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+        >
+            <path
+                fill="currentColor"
+                d="M3 6h10v2H3zm0 10h10v2H3zm0-5h12v2H3zm13-4l-1.42 1.39L18.14 12l-3.56 3.61L16 17l5-5z"
+            ></path>
+        </svg>
+    );
+}
+
+function OpenedMenu() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+        >
+            <path
+                fill="currentColor"
+                d="M3 18v-2h13v2zm16.6-1l-5-5l5-5L21 8.4L17.4 12l3.6 3.6zM3 13v-2h10v2zm0-5V6h13v2z"
+            ></path>
+        </svg>
+    );
+}
