f557908b78cf025a20a9a129af2a8807306f9f30
Ethan Glenn <ethan+git@eglenn.dev>
2025-04-16 13:18:22 -0600

    Removed old help message on the home page

--- app/(authenticated)/home/post-list.tsx
+++ app/(authenticated)/home/post-list.tsx
@@ -298,20 +298,6 @@ export default function PostList({ categories, initialPosts }: PostListProps) {
                     />
                 </div>
                 <div className="flex items-center space-x-4 w-full sm:w-auto justify-between sm:justify-end">
-                    <div className="hidden md:flex items-center space-x-2 text-gray-500">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width={24}
-                            height={24}
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                fill="currentColor"
-                                d="M12 22q-.825 0-1.412-.587T10 20h4q0 .825-.587 1.413T12 22m-4-3v-2h8v2zm.25-3q-1.725-1.025-2.738-2.75T4.5 9.5q0-3.125 2.188-5.312T12 2t5.313 2.188T19.5 9.5q0 2.025-1.012 3.75T15.75 16zm.6-2h6.3q1.125-.8 1.738-1.975T17.5 9.5q0-2.3-1.6-3.9T12 4T8.1 5.6T6.5 9.5q0 1.35.613 2.525T8.85 14M12 14"
-                            ></path>
-                        </svg>
-                        <p>Try right clicking a card</p>
-                    </div>
                     <div className="flex items-center border rounded-md overflow-hidden mr-2">
                         <Button
                             variant={viewType === "grid" ? "default" : "ghost"}
